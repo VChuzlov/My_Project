be0ebb04c378d59f62f8841235ccb7de6203ee94
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2022-04-04 23:28:21 +0700

    to fugasity calculation step

--- py-model/PRmodel.py
+++ py-model/PRmodel.py
@@ -1,5 +1,5 @@
 """
-    The Decribtion of Peng-Robinson model.
+    The Description of Peng-Robinson model.
 """
 
 import constants as const
@@ -7,76 +7,107 @@ import numpy as np
 from scipy.optimize import fsolve
 
 
-def get_component_pressure_by_pr(t, v, tc, pc, omega, kij, x):
+def p_sat_by_wilson(t, pc, tc, omega):
     """
-    Peng-Robinson equation for component pressure
-    :param t: <float> temperature
-    :param v: <float> molar volume
-    :param tc: <float> critical temperature
-    :param pc: <float> critical pressure
-    :param omega: <float> acentric factor
-    :return: <float> component pressure
+    Wilson equation for Psat calculation/
+    :param t: K
+    :param pc: Pa
+    :param tc: K
+    :param omega:
+    :return:
     """
-    b_i = [0.077796 * const.R * t / p for t, p in zip(tc, pc)]
-    b = sum(b * xi for b, xi in zip(b_i, x))
+    return [pci * np.exp(5.373 * (1 - omi) * (1 - tci / t))
+            for pci, omi, tci in zip(pc, omega, tc)]
 
-    ac_i = [0.457235 * (const.R * t) ** 2 / p for t, p in zip(tc, pc)]
-    m_i = [0.37646 + 1.54226 * om - 0.26992 * om ** 2 if om <= 0.49
-           else 0.379642 + (1.48503 - (0.164423 - 1.016666 * om) * om) * om
-           for om in omega]
-    alpha_i = [(1 + m * (1 - (t / t_c) ** 0.5)) ** 2 for m, t_c in zip(m_i, tc)]
-    a_i = [ac * alpha for ac, alpha in zip(ac_i, alpha_i)]
 
-    aij = [[(1 - kij[i][j]) * (a_i[i] * a_i[j]) ** 0.5 for j in range(const.COMP_COUNT)]
-           for i in range(const.COMP_COUNT)]
+def rachford_rice(e, zi, ki):
+    """
+    Rachford-Rice equation
+    :param e: mole fraction of vapor phase
+    :param zi: components mole fractions
+    :param ki: phase equilibrium constant
+    :return:
+    """
+    return sum(z * (k - 1) / (e * (k - 1) + 1)
+               for z, k in zip(zi, ki))
 
-    a = 0
+
+def first_guess(t, p, zi, pc, tc, omega):
+    psat_i = p_sat_by_wilson(t, pc, tc, omega)
+
+    ki = [psat / p for psat in psat_i]
+
+    e0 = np.array([0.5])
+    e, *_ = fsolve(rachford_rice, e0, args=(zi, ki))
+
+    s1 = sum(z * k for z, k in zip(zi, ki))
+    s2 = sum(z / k for z, k in zip(zi, ki))
+
+    xi = [0 for _ in range(const.COMP_COUNT)]
+    yi = [0 for _ in range(const.COMP_COUNT)]
+
+    if s1 < 1:
+        xi = zi[:]
+
+    elif s2 < 1:
+        yi = zi[:]
+
+    else:
+        xi = [z / (1 + e * (k - 1)) for z, k in zip(zi, ki)]
+        yi = [z * k / (1 + e * (k - 1)) for z, k in zip(zi, ki)]
+
+    return xi, yi
+
+
+def second_guess(t, p, xi, yi, tc, pc, omega, kij):
+    a0, b0 = 0.457235, 0.077796
+
+    bi = [b0 * const.R * tci / pci for tci, pci in zip(tc, pc)]
+    bv = sum(y * b for y, b in zip(yi, bi))
+    bl = sum(x * b for x, b in zip(xi, bi))
+
+    ni = [0.37646 + 1.5422 * omi - 0.26992 * omi ** 2 if omi <= 0.49
+          else 0.379642 + (1.48503 - (0.164423 - 1.016666 * omi) * omi) * omi
+          for omi in omega]
+
+    alphai = [(1 + n * (1 - (t / tci) ** 0.5)) ** 2
+              for n, tci in zip(ni, tc)]
+
+    aci = [a0 * (const.R * tci) ** 2 / pci
+           for tci, pci in zip(tc, pc)]
+
+    ai = [a * alph for a, alph in zip(aci, alphai)]
+
+    av, al = 0, 0
     for i in range(const.COMP_COUNT):
         for j in range(const.COMP_COUNT):
-            a += aij[i][j] * x[i] * x[j]
+            av += yi[i] * yi[j] * (ai[i] * ai[j]) ** 0.5 * (1 - kij[i][j])
+            al += xi[i] * xi[j] * (ai[i] * ai[j]) ** 0.5 * (1 - kij[i][j])
 
-    return [const.R * t / (vi - b) - a / (vi * (vi + b) + b * (vi - b))
-            for vi in v]
+    aav = av * p * (const.R * t) ** 2
+    aal = al * p / (const.R * t) ** 2
+
+    bbv = bv * p / (const.R * t)
+    bbvl = bl * p / (const.R * t)
+
+    return aav, aal, bbv, bbvl
 
 
-def get_zfactor(t, v, tc, pc, omega, kij, x):
-    """
-    Peng-Robinson equation for Z-factor calculation
-    :param t: <float> temperature
-    :param v: <ndarray> molar volume
-    :param tc: <ndarray> critical temperature
-    :param pc: <ndarray> critical pressure
-    :param omega: <ndarray> acentric factor
-    :param kij: <ndarray> Peng-Robinson params
-    :param x: <ndarray> components molar fractions
-    :return: <float> Z-factor
-    """
-    b_i = 0.0077796 * const.R * tc / pc
-    ac_i = 0.457235 * (const.R * tc) ** 2 / pc
-    m_i = 0.37646 + 1.54226 * omega - 0.26992 * omega ** 2 if omega <= 0.49 \
-        else 0.379642 + (1.48503 - (0.164423 - 1.016666 * omega) * omega) * omega
-    alpha_i = 1 + m_i * (1 - (t / tс) ** 0.5) ** 2
-    a_i = ac_i * alpha_i
+def cubic_pr(z, a, b):
+    return (z ** 3 + (b - 1) * z ** 2
+            + (a - 2 * b - 3 * b ** 2) * z
+            + (b ** 2 + b ** 3 - a * b))
 
-    p = const.R * t / (v - b_i) - a_i / (v * (v + b_i) + b_i * (v - b_i))
 
-    b = (x * b_i).sum()
-    a = 0
-    for i in range(x.shape[0]):
-        s = 0
-        for j in range(x.shape[0]):
-            s += x[i] * x[j] + (a_i[i] * a_i[j]) ** 0.5 * (1 - kij[i][j])
-        a += s
+def get_z(foo, z, a, b, flag):
+    z0 = np.array([0.1, 0.5, 1.0])
+    sol = fsolve(foo, z0, args=(a, b))
 
-    A = a * p / (const.R * t) ** 2
-    B = b * p / (const.R * t)
+    if flag == 'l':
+        return sol[np.where(sol > 0)].min()
 
-    equation = lambda z: z ** 3 + (1 - B) * z ** 2 \
-                         + (A - 2 * B - 3 * B ** 2) * z \
-                         - (A * B - B ** 2 - B ** 3)
-
-    z = fsolve(equation, np.array([0, 0.5, 1]))
-    return z.min()
+    if flag == 'v':
+        return sol[np.where(sol > 0)].max()
 
 
 class PRSolution:
