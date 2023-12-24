ccb55627b35c446cf5b1f3f7682a36ad06d2b52e
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2023-12-24 22:50:26 +0700

    DewPoint Project

---
+++ DewPoint-project/TypesUnit.pas
@@ -0,0 +1,13 @@
+unit TypesUnit;
+
+interface
+
+type
+  TArrOfDouble = array of Double;
+  TMatrixOfDouble = array of array of Double;
+  TFoo = function(x: TArrOfDouble): Double;
+  TCubicEquationMethod = function(a, b, c: Double): TArrOfDouble;
+
+implementation
+
+end.
