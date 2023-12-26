a69b4bcd43501247e04eac08371fcab7aa726717
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2023-12-26 14:15:52 +0700

    Try to Run

--- DewPoint-project/Unit1.pas
+++ DewPoint-project/Unit1.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, ConvertersUnit, Vcl.StdCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, ConvertersUnit, TypesUnit, DewPointUnit,
+  Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
@@ -24,11 +25,37 @@ implementation
 {$R *.dfm}
 
 procedure TForm1.Button1Click(Sender: TObject);
+const
+  Tc: TArrOfDouble = [
+    -82.45099487,  32.27800903, 96.74801025, 134.9460083,  152.04900513,
+    187.24801025, 196.4500061, 234.74801025, 267.00802002, 295.44802246
+  ];
+  Pc: TArrOfDouble = [
+    4640.68017578, 4883.85009766, 4256.66015625, 3647.62011719, 3796.62011719,
+    3333.59008789, 3375.12011719, 3031.62011719, 2736.7800293,  2496.62011719
+  ];
+  Af: TArrOfDouble = [
+    0.0114984,  0.0986,     0.1524,     0.18479,   0.20100001, 0.22224,
+    0.25389001, 0.30070001, 0.34979001, 0.40180001
+  ];
+  Vc: TArrOfDouble = [
+    0.0989999,  0.148,      0.2,        0.26300001, 0.25499001, 0.30799001,
+    0.31099001, 0.368,      0.42598,    0.486
+  ];
+  Pressure: Double = 201.325;
+  Yi: TArrOfDouble = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1];
+
 var
   uc: TUnitsConverter;
+  dp: TDewPoint;
+  TDew: Double;
 begin
   uc := TUnitsConverter.Create();
-  ShowMessage(FloatToStr(uc.Temperature.RankineToCelsius(655.1282815742853)))
+  dp := TDewPoint.Create(
+    Pressure, Yi, Tc, Pc, Af, Vc
+  );
+  TDew := dp.Calculation();
+  ShowMessage(FloatToStr(uc.Temperature.RankineToCelsius(TDew)));
 end;
 
 end.
