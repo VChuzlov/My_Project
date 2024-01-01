1fd3b2cd3af74607ad7a5894442f2a812a452db5
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2024-01-01 23:29:34 +0700

    BrentsMethod

--- DPCpp/DewPointCpp/Functions.h
+++ DPCpp/DewPointCpp/Functions.h
@@ -1,3 +1,7 @@
 #include <vector>
+#include <functional>
 
 std::vector<double> VietaMethod(double, double, double);
+double BrentsMethod(std::function<double (double)> f, 
+    double lower, double upper, double tol, 
+    unsigned int MaxIter);
