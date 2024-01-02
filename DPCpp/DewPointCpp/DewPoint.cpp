e13c1d4c76f71c7d317aead6304cd46793dd86e6
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2024-01-02 18:00:30 +0700

    Update DewPoint.cpp

--- DPCpp/DewPointCpp/DewPoint.cpp
+++ DPCpp/DewPointCpp/DewPoint.cpp
@@ -7,13 +7,29 @@ double DewPoint::CalculateAalpha(std::vector<double> mf,
     std::vector<double> alpha)
 {
     double Result = 0.0;
-    for (int i = 0; i < mf.size(); ++i)
+    for (unsigned int i = 0; i < mf.size(); ++i)
     {
-        for (int j = 0; j < mf.size(); ++j)
+        for (unsigned int j = 0; j < mf.size(); ++j)
         {
             Result += (
                 mf[i] * mf[j] * (1 - kij[i][j])
                 * pow(ai[i] * alpha[i] * ai[j] * alpha[j], 0.5));
+        }
+    }
+    return Result;
+}
+
+std::vector<std::vector<double>> DewPoint::CalculateAb(
+    std::vector<std::vector<double>> kij, std::vector<double> ap)
+{
+    unsigned int size = ap.size();
+    std::vector<std::vector<double>> Result(size);
+    for (unsigned int i = 0; i < size; ++i)
+    {
+        Result[i].resize(size);
+        for (unsigned int j = 0; j < size; ++j)
+        {
+            Result[i][j] = (1 - kij[i][j]) * pow(ap[i] * ap[j], 0.5);
         }
     }
     return Result;
